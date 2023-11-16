d34d87e73e396913240e935635435e0fde5df7b3
kaitbaer <122306622+kaitbaer@users.noreply.github.com>
2023-11-16 00:47:02 +0000

    Starting React App and adding First two components

---
+++ stress-free-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./Components/NavBar"
+import CenterContainer from './Components/CenterContainer';
+
+
+function App() {
+  return (
+  <>
+    <NavBar />
+    <CenterContainer />
+  </>
+  );
+}
+
+export default App;
